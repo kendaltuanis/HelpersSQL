8edb11a5ccdabbf87eb882fccd6ba332d24c4e1c
kendal <kendaltuanis@hotmail.com>
2017-03-14 15:29:11 -0600

    Liberia 1.0 Helpers

---
+++ HelpersSQL/SqlHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpersSQL
+{
+    public class SqlHelper
+    {
+        private string table;
+        private const string SEPARATOR = ",";
+        private const bool WHERECONDITION = true;  // TRUE es 'and' y FALSE es 'or'
+
+        public SqlHelper(String table)
+        {
+            this.table = table;
+        }
+
+        public string InsertSql(string[] fields)
+        {
+            return string.Format("INSERT INTO {0} ({1}) VALUES ({2})", table, JoinFields(fields), JoinFieldWithAt(fields));
+        }
+
+        public string UpdateSql(string[] fields, string[] where, bool andCondition = WHERECONDITION)
+        {
+            return string.Format("UPDATE {0} SET {1} WHERE {2}", table, JoinFieldsWithEquals(fields), Where(where, andCondition));
+        }
+
+        public string DeleteSql(string[] whereFields = null, bool andCondition = WHERECONDITION)
+        {
+            return string.Format("DELETE FROM {0} " + ((whereFields != null) ? " WHERE {1}" : ";"), table, Where(whereFields, andCondition));
+        }
+
+        public string SelectSql(string[] fields)
+        {
+            return string.Format("SELECT {0} FROM {1}" + ((fields != null) ? " WHERE {2}" : ";"), JoinFields(fields), table, JoinFieldsWithEquals(fields));
+        }
+
+        /*
+         Método que se encarga de retornar en un 'string' unido de los campos separados por el signo
+          que se requiera. DEFAULT SIGNO = ","
+          Ej: "campo1,campo2,campo3"
+         */
+        private string JoinFields(string[] fields, string signo = SEPARATOR)
+        {
+            return string.Join(signo, fields);
+        }
+
+        /*
+         Método que se encarga de retornar en un 'string' los campos separados por el signo
+          que se requiera, además con los campos igualados y las '@' por delante. DEFAULT SIGNO = ","
+           Ej: "campo1=@campo1,campo2=@campo2,campo3=@campo3"
+         */
+        private string JoinFieldsWithEquals(string[] fields, string signo = SEPARATOR)
+        {
+            return (fields == null) ? " " : string.Join(signo, fields.Select(s => s + "=" + "@" + s));
+        }
+
+        /*
+         Método que se encarga de retornar en un 'string' los campos separados por el signo
+          que se requiera y además con las '@' por delante. DEFAULT SIGNO = ","
+           Ej: "@campo1,@campo2,@campo3"
+         */
+        private string JoinFieldWithAt(String[] fields)
+        {
+            return string.Join(",", fields.Select(s => "@" + s));
+        }
+
+        /*
+         Método que se encarga de retornar en un 'string' los campos separados por el signo
+          que se requiera, además con los campos igualados y las '@' por delante. DEFAULT whereConditions = true (and)
+           Ej: "campo1=@campo1 and campo2=@campo2 and campo3=@campo3" || "campo3=@campo1 or campo=@campo2"
+         */
+        private string Where(String[] whereFields, bool andCondition)
+        {
+            String caracter = ((andCondition) ? " and " : " or ");
+            return string.Join(caracter, JoinFieldsWithEquals(whereFields, caracter));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-# Providers de conexión a base de datos 
+# Librería Helpers para C#
 
-| Provider  | connection string example |
+# Actualizar con el repositorio original 
 | ------------- | ------------- |
-| Postgres  | Server=server;Port=5432;User Id=postgres;Password=secret;Database=dbname;CommandTimeout=3600;  |
-| SqlServer | data source=server;initial catalog=dbname;user id=user;password=password;  |
-| MsAccess | Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Path\To\Database\File.accdb; Persist Security Info=False;  |
-| SQLite |Data Source=C:\Users\MORA\Desktop\banco.db;Version=3; |
-| SqlServerAccessODBC | Server=localhost;Port=5433;User Id=user;Password=password;Database=dbname;CommandTimeout=3600; |
-| MySql | Server=myServerAddress; Port=3306; Database=myDataBase; Uid=myUsername; Pwd=myPassword;  |
+| 1: | git remote add upstream https://github.com/kendaltuanis/practicahelpers |
+| 2: | git fetch upstream |
+| 3: | git merge upstream/master |
+| 4: | git pull |
 
+# Realizar commits
+| ------------- | ------------- |
+| 1: | git status | Ve todos los cambios que fueron hechos en su proyecto |
+| 2: | git add "nombreCambioArchivo" | Se agrega el cambio que se haya realizado |
+| 3: | git commit -m "nombredelcommit" | Se agrega el commit con sus respectivo nombre|
+| 4: | git push | Para subir el archivo|
+| 5: | PULL REQUEST | ESTO SE REALIZA DESDE LA PÁGINA |
 
-# Actualizar con el repositorio original
-| git remote add upstream https://github.com/kendaltuanis/practicahelpers  
-| git fetch upstream | git merge upstream/master | git pull
 
 
 
