60bdf7ee672d2bcdff0bd811e01919dc1d4b5b05
kendal <kendaltuanis@hotmail.com>
2017-03-13 19:12:38 -0600

    Utils

---
+++ PracticaHelpers/Utils/SqlHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaHelpers.Utils
+{
+   
+        class SqlHelper
+        {
+            public string InsertSql(string table, string[] fields)
+            {
+            StringBuilder sql = new StringBuilder();
+            sql.Append("INSERT INTO ").Append(table).Append(" VALUES(");
+            foreach (string field in fields)
+            {
+                sql.Append(field).Append(","); 
+            }
+
+
+                return "";
+            }
+
+            public string UpdateSql(string table, string[] fields)
+            {
+                return "";
+            }
+
+            public string DeleteSql(string table, string[] fields)
+            {
+                return "";
+            }
+
+            public string SelectSql(string table, string[] fields)
+            {
+                return "";
+            }
+        }
+
+    }
+
